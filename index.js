28a4e5f80137d595a72493b0cd29a3be870377ed
Mahmudjon Shukurov <sukurovmahmudzon15@gmail.com>
2024-05-15 09:38:34 +0300

    tugadi

---
+++ index.js
@@ -0,0 +1,9 @@
+let ism = prompt("Ismingizni kiriting");
+let id = prompt("id kiriting");
+let parol = prompt("parolni kiriting ");
+
+let data = {
+    ism: ism,
+    id: id,
+    parol: parol,
+};
